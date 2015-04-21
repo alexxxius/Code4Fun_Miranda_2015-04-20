efea7bf4c604a4d0b05284a6c173e700a28593df
alexxxius <alemiranda79@gmail.com>
2015-04-21 12:43:44 +0200

    Add - file not tracked

---
+++ TDDProblem/FilesBinRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDDProblem
+{
+    public class FilesBinRepository : IFilesBinRepository
+    {
+        public IList<string[,]> LoadFilesFromPath(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(string path, IList<string> tsvStrings)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDDProblem/IFilesBinRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDDProblem
+{
+    public interface IFilesBinRepository
+    {
+        IList<string[,]> LoadFilesFromPath(string path);
+        bool Save(string path, IList<string> tsvStrings);
+
+    }
+}
